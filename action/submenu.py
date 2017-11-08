d13784c7cc94bf5f67916a74300e3fec2918f9a3
Olga Boriskina <boriskina1992@gmail.com>
2017-11-09 01:32:33 +0300

    update actions

--- action/submenu.py
+++ action/submenu.py
@@ -3,17 +3,26 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as expected
 from selenium.webdriver.common.by import By
 
-menu_locator = "//a[contains(.,'Courses')]"
-submenu_locator = "//ul[@class='dropdown-menu']//a[@href='Jmeter Training/Jmeter_training_noida_delhi.html']"
+menu_locator = "id('ui-id-2')"
+submenu_locator = "id('ui-id-3')"
+frame_locator = "//iframe[@src='menu/defult2.html']"
+menu_with_sub_locator = "//a[@href='#example-1-tab-2']"
+
 
 class ActionSubmenu:
     def __init__(self, driver):
-        self.__driver = driver
-        self.__wait = WebDriverWait(self.__driver, 10)
+        self._driver = driver
+        self._wait = WebDriverWait(self._driver, 10)
+
+    def switch_to_frame(self):
+        frame = self._driver.find_element_by_xpath(frame_locator)
+        self._driver.switch_to.frame(frame)
 
     def action_submenu(self):
-        element = self.__wait.until(expected.element_to_be_clickable((By.XPATH, menu_locator)))
-        actions = ActionChains(self.__driver)
+        self._wait.until(expected.element_to_be_clickable((By.XPATH, menu_with_sub_locator))).click()
+        self.switch_to_frame()
+        element = self._wait.until(expected.element_to_be_clickable((By.XPATH, menu_locator)))
+        actions = ActionChains(self._driver)
         actions.move_to_element(element).perform()
-        submenu_element = self.__wait.until(expected.element_to_be_clickable((By.XPATH, submenu_locator)))
+        submenu_element = self._wait.until(expected.element_to_be_clickable((By.XPATH, submenu_locator)))
         return submenu_element
