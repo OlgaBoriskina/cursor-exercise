8ed3b09e6cc6be085b74b5202ca4017c2c41ead7
Olga Boriskina <boriskina1992@gmail.com>
2017-11-09 01:33:38 +0300

    update main

--- main/main.py
+++ main/main.py
@@ -2,15 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from submenu import ActionSubmenu
 
+import cookie_autorization
 import unittest
 
-base_url = "http://way2automation.com/"
+base_url = "http://way2automation.com/way2auto_jquery"
+
 
 class Test (unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome("chromedriver.exe")
         self.driver.get(base_url)
         self.wait = WebDriverWait(self.driver, 10)
+        cookie_autorization.autirize(self.driver)
+        self.driver.get(base_url + "/menu.php")
 
     def test_action_submenu(self):
         submenu = ActionSubmenu(self.driver)
