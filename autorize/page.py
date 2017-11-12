35603fcf96548bc666d3f400a3b3006bd45cb51f
Olga Boriskina <boriskina1992@gmail.com>
2017-11-12 13:35:28 +0300

    update authorize

--- autorize/page.py
+++ autorize/page.py
@@ -5,25 +5,23 @@ login_locator = "//a[@href='#login']"
 user_locator = "id('login')//input[@name='username']"
 password_locator = "id('login')//input[@name='password']"
 submit_locator = "id('login')//input[@class='button']"
-home_lacator = "//a[@href='http://www.way2automation.com']"
 
 
 def login(wait):
     login = wait.until(expected.element_to_be_clickable((By.XPATH, login_locator)))
     return login
 
+
 def user(wait):
-    user =  wait.until(expected.element_to_be_clickable((By.XPATH, user_locator)))
+    user = wait.until(expected.element_to_be_clickable((By.XPATH, user_locator)))
     return user
 
-def passw (wait):
+
+def password(wait):
     password = wait.until(expected.element_to_be_clickable((By.XPATH, password_locator)))
     return password
+
 
 def submit(wait):
     click_submit = wait.until(expected.element_to_be_clickable((By.XPATH, submit_locator)))
     return click_submit
-
-def autirize (wait):
-    wait.until(expected.element_to_be_clickable((By.XPATH, submit_locator)))
-    return submit_locator
